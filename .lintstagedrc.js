daabf6eaf7ddd59d496bf1a67952591c9a6a6982
Darnell Noel <darnellnoel.webdev@gmail.com>
2023-07-28 00:53:42 -0400

    chore: update config files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,15 +2,16 @@
 
 const path = require('path');
 
-const handleFiles = (filenames) =>
-    filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ');
+const handleFiles = (filenames, separator = ' --file ') =>
+    filenames.map((f) => path.relative(process.cwd(), f)).join(separator);
 
 const buildEslintCommand = (filenames) =>
     `next lint --fix --file ${handleFiles(filenames)}`;
 
-const prettierCommand = (filenames) => `npx prettier --write ${handleFiles(filenames)}`;
+const prettierCommand = (filenames) =>
+    `npx prettier --write ${handleFiles(filenames, ' ')}`;
 
 module.exports = {
-    '*.{js,jsx,ts,tsx}': [buildEslintCommand, prettierCommand],
+    '*.{js,jsx,ts,tsx}': [prettierCommand, buildEslintCommand],
     '*.{md,json}': [prettierCommand],
 };
--- next.config.js
+++ next.config.js
@@ -1,29 +1,68 @@
+/** @format */
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const withRoutes = require('nextjs-routes/config')();
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
+
 /**
- * @format
  * @type {import('next').NextConfig}
  */
-
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    modularizeImports: {
+        '@mui/material': {
+            transform: '@mui/material/{{member}}',
+        },
+        '@mui/icons-material': {
+            transform: '@mui/icons-material/{{member}}',
+        },
+        '@mui/styles': {
+            transform: '@mui/styles/{{member}}',
+        },
+        '@mui/lab': {
+            transform: '@mui/lab/{{member}}',
+        },
+    },
     onDemandEntries: {
         // Period (in ms) where the server will keep pages in the buffer
         maxInactiveAge: 600 * 1000,
         // Number of pages that should be kept simultaneously without being disposed
         pagesBufferLength: 15,
     },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'src/styles/sass')],
+    },
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
     webpack: (config, { isServer }) => {
         // Fixes npm packages that depend on `fs` module
-        const updatedConfig = config;
         if (!isServer) {
-            updatedConfig.resolve.fallback = {
-                ...updatedConfig.resolve.fallback,
+            config.resolve.fallback = {
+                ...config.resolve.fallback,
                 fs: false,
+                path: false,
+                worker_threads: false,
             };
         }
 
-        return updatedConfig;
+        // https://react-svgr.com/docs/next/#usage
+
+        return config;
     },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(withRoutes(nextConfig));
